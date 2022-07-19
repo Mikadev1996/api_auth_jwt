ea997139826c36eb8e66e3c1685c5d33de791879
mika_laptop_windows <charmikadev@gmail.com>
2022-07-19 11:59:25 +0100

    jwt token auth

--- routes/index.js
+++ routes/index.js
@@ -13,15 +13,53 @@ router.get('/api', (req, res) => {
   })
 })
 
-router.post('/api/posts', (req, res) => {
-  res.json({
-    message: 'Post Created...'
+router.post('/api/posts', verifyToken, (req, res, next) => {
+  jwt.verify(req.token, 'secretkey', (err, authData) => {
+    if (err) {
+      res.sendStatus(403).json({error: err});
+    } else {
+      res.json({
+        message: 'Post Created...',
+        authData: authData,
+
+      })
+    }
   })
 })
 
-router.post('/api/login', (res, res) => {
+router.post('/api/login', (res, res, next) => {
   // Mock user
-  jwt.sign();
+  const user = {
+    id: 1,
+    username: 'testName',
+    email: 'test@gmail.com'
+  }
+  jwt.sign({user: user}, 'secretkey', {expiresIn: '3600s'} ,(err, token) => {
+    if (err) return next(err);
+    res.json({
+      token: token,
+    })
+  });
 })
+
+// Token Format
+// Authorization: Bearer <access_token>
+
+// Verify token
+
+function verifyToken(req, res, next) {
+  // Get auth header value
+  const bearerHeader = req.headers['authorization'];
+  // Check if undefined
+  if (typeof bearerHeader !== 'undefined') {
+    // Split at space
+    const bearer = bearerHeader.split(' ');
+    req.token = bearer[1];
+    next();
+  } else {
+    res.send(403).json({error: 'Token undefined'})
+  }
+
+}
 
 module.exports = router;
